4b9e69407147b52bff59291b888b2f9dcd151213
Oleg Polyakov <oleg@webmastersdv.com>
2024-03-26 20:44:16 +1000

    Рефактрониг формы создания новой заявки

--- resources/js/Domain/Bid/Components/CreateBid.vue
+++ resources/js/Domain/Bid/Components/CreateBid.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
 
 const bid = reactive({
@@ -8,25 +8,83 @@ const bid = reactive({
     text: '',
 })
 
+const bidIsCreating = ref(false)
+
+const validate = computed(() =>
+    bid.name.trim().length > 0 &&
+    bid.telephone.trim().length > 0 &&
+    bid.text.trim().length > 0
+)
+
 function createNewBid() {
+    if (!validate.value || bidIsCreating.value) return
+
+    bidIsCreating.value = true
+
     window.axios.post('/api/bid', bid)
         .then(function (response) {
-            console.log(response)
+            if (!response?.data?.success) return
+
+            bid.name = ''
+            bid.telephone = ''
+            bid.text = ''
+
+            bidIsCreating.value = false
+
+            console.log(response.data.success)
         })
         .catch(function (error) {
+            bidIsCreating.value = false
             console.log(error)
         })
 }
 </script>
 
 <template>
-    <form @submit.prevent="createNewBid">
-        <input type="text" v-model="bid.name">
+    <form :class="$style.form">
+        <label>Имя:</label>
+        <input type=text placeholder="Введите имя..." v-model="bid.name">
 
-        <input type="text" v-model="bid.telephone">
+        <label>Телефон:</label>
+        <input type=text placeholder="Введите номер телефона..." v-model="bid.telephone">
 
-        <input type="text" v-model="bid.text">
+        <label>Обращение:</label>
+        <input type=text placeholder="Введите текст заявки..." v-model="bid.text">
 
-        <button type=button @click="createNewBid">Отправить заявку</button>
+        <button type=button :disabled="!validate || bidIsCreating" @click="createNewBid">{{ bidIsCreating ? 'Отправка...' : 'Сохранить' }}</button>
     </form>
 </template>
+
+<style module lang="scss">
+.form {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    max-width: 640px;
+    margin: 0 auto;
+    border-radius: 8px;
+    box-shadow: 4px 4px 32px #0000005b;
+
+    input,
+    button {
+        padding: 8px;
+        border: solid 1px;
+        border-radius: 4px;
+    }
+
+    input {
+        margin-top: 4px;
+        margin-bottom: 16px;
+    }
+
+    button {
+        margin-top: 16px;
+        font-size: 24px;
+        cursor: pointer;
+
+        &:disabled {
+            cursor: not-allowed;
+        }
+    }
+}
+</style>
